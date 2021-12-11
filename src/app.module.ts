39cebe630f6375e41819b1f2c1228b0438f0746e
Ramprit Sahani <palak.ghai@torinit.ca>
2021-12-11 17:08:00 +0530

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from './auth/auth.module';
+import { RoleModule } from './role/role.module';
+import { PermissionModule } from './permission/permission.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "plumtree",
+    database: "microserviceDB",
+    autoLoadEntities: true,
+    synchronize: true
+  }), UserModule, AuthModule, RoleModule, PermissionModule, CommonModule],
+  controllers: [],
+  providers: []
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: "*",
+    credentials: true
+  });
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap().then(() => console.log("running app..."));
